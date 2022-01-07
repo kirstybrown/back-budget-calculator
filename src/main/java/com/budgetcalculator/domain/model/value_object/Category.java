1e1bc023c9d346410e5329477273c417f0dbd864
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-07 12:27:08 +0100

    refactor: changed order of enum values so first (default) is 'other'
    refer: create-mysql-db#9

--- src/main/java/com/budgetcalculator/domain/model/value_object/Category.java
+++ src/main/java/com/budgetcalculator/domain/model/value_object/Category.java
@@ -1,10 +1,10 @@
 package com.budgetcalculator.domain.model.value_object;
 
 public enum Category {
+    OTHER,
     FOOD,
     BILLS,
     TRANSPORT,
     RENT,
-    PURCHASES,
-    OTHER
+    PURCHASES
 }
