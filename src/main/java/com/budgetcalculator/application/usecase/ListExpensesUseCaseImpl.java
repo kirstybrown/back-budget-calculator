9fb992353eca094dc8eb604fbf1696c0bea50a7a
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-07 21:58:01 +0100

    feature: added expense list use case implementation for findall()
    refer: create-mysql-db#9

--- src/main/java/com/budgetcalculator/application/usecase/ListExpensesUseCaseImpl.java
+++ src/main/java/com/budgetcalculator/application/usecase/ListExpensesUseCaseImpl.java
@@ -1,62 +1,52 @@
 package com.budgetcalculator.application.usecase;
 
 import com.budgetcalculator.application.query_service.ListExpensesUseCase;
+import com.budgetcalculator.application.repository.ExpenseRepository;
 import com.budgetcalculator.domain.model.aggregate.Expense;
-import com.budgetcalculator.domain.model.value_object.Category;
-import com.budgetcalculator.domain.model.value_object.Currency;
-import com.budgetcalculator.domain.model.value_object.Money;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.management.RuntimeErrorException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @NoArgsConstructor
 public class ListExpensesUseCaseImpl implements ListExpensesUseCase {
 
-    @Override
-    public List<Expense> listExpenses() {
-        return mockFindAllExpensesInDatabase().orElse(new ArrayList<>());
+    private final ExpenseRepository expenseRepository;
+
+    public ListExpensesUseCaseImpl(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
     }
 
     @Override
-    public Expense findExpense(Long expenseId) {
-        return mockFindExpenseByIdInDatabase(expenseId).get();
+    @Transactional(readOnly = true)
+    public List<Expense> findAll() {
+        List<Expense> expenseList = StreamSupport
+                .stream(this.expenseRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+
+        return expenseList;
+
     }
 
-    public Optional<Expense> mockFindExpenseByIdInDatabase(Long expenseId) {
-        Expense expense = new Expense(3L, "ice cream",
-                new Money(Currency.EURO, 5.5), Category.FOOD);
-
-        if (expenseId == 1) {
-            expense = new Expense(1L, "pizza",
-                    new Money(Currency.EURO, 10.5),
-                    Category.FOOD);
+    @Override
+    @Transactional(readOnly = true)
+    public Expense findById(Long expenseId) {
+        Optional<Expense> expense = this.expenseRepository.findById(expenseId);
+        if(expense.isPresent()) {
+            return this.expenseRepository.findById(expenseId).get();
         }
-        if (expenseId == 2) {
-            expense = new Expense(2L, "diesel",
-                    new Money(Currency.EURO, 80.1),
-                    Category.TRANSPORT);
-        }
-
-        return Optional.of(expense);
+        return null;
     }
 
-    public Optional<List<Expense>> mockFindAllExpensesInDatabase() {
-        List<Expense> listExpenses = new ArrayList<>();
-        listExpenses.add(new Expense(
-                1L,
-                "pizza",
-                new Money(Currency.EURO, 10.5),
-                Category.FOOD));
-        listExpenses.add(new Expense(
-                2L,
-                "diesel",
-                new Money(Currency.EURO, 80.1),
-                Category.TRANSPORT));
-        return Optional.of(listExpenses);
+    @Override
+    @Transactional
+    public Expense save(Expense expense) {
+        return this.expenseRepository.save(expense);
     }
 }
