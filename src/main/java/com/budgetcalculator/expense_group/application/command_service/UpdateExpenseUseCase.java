226b494f9f8756c990463c609f3df93c65b5ccb3
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-07-25 16:17:40 +0200

    feat: created Update Expense UseCase and implementation.

---
+++ src/main/java/com/budgetcalculator/expense_group/application/command_service/UpdateExpenseUseCase.java
@@ -0,0 +1,8 @@
+package com.budgetcalculator.expense_group.application.command_service;
+
+import com.budgetcalculator.expense_group.domain.model.aggregate.Expense;
+
+public interface UpdateExpenseUseCase {
+
+    void updateExpense(Expense updatedExpense);
+}
---
+++ src/main/java/com/budgetcalculator/expense_group/application/usecase/UpdateExpenseUseCaseImpl.java
@@ -0,0 +1,11 @@
+package com.budgetcalculator.expense_group.application.usecase;
+
+import com.budgetcalculator.expense_group.application.command_service.UpdateExpenseUseCase;
+import com.budgetcalculator.expense_group.domain.model.aggregate.Expense;
+
+public class UpdateExpenseUseCaseImpl implements UpdateExpenseUseCase {
+    @Override
+    public void updateExpense(Expense updatedExpense) {
+
+    }
+}
