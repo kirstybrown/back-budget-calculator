39917f5b63770817964088eaa318151f1ef1ebd1
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2022-08-19 17:01:04 +0200

    fix: added dependency with constructor

--- src/main/java/com/budgetcalculator/expense_group/application/usecase/DeleteExpenseUseCaseImpl.java
+++ src/main/java/com/budgetcalculator/expense_group/application/usecase/DeleteExpenseUseCaseImpl.java
@@ -2,12 +2,14 @@ package com.budgetcalculator.expense_group.application.usecase;
 import com.budgetcalculator.expense_group.application.command_service.DeleteExpenseUseCase;
 import com.budgetcalculator.expense_group.application.repository.ExpenseRepository;
 import com.budgetcalculator.expense_group.domain.model.aggregate.ExpenseId;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DeleteExpenseUseCaseImpl implements DeleteExpenseUseCase {
 
-    private ExpenseRepository expenseRepository;
+    private final ExpenseRepository expenseRepository;
 
     @Override
     public void deleteExpenseById(ExpenseId expenseId) {
